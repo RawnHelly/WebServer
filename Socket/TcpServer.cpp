3c94b2405a24efe816b4c1abdd7b1a60c13203db
Rawn <liu_826475@163.com>
2023-08-20 22:46:05 +0800

    创建了一个类TcpSocket，用于创建一个套接字

---
+++ Socket/TcpServer.cpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<cstdlib>
+#include<cstring>
+#include<iostream>
+
+#include "Log.hpp"
+
+class TcpServer {
+private:
+    int port;
+    int listen_sock;
+    static TcpServer *svr;
+private:
+    TcpServer(int _port): port(_port) , listen_sock(-1) {}
+    TcpServer(const TcpServer &s){}
+public:
+    static TcpServer *getinstance(int port) { //单例模式
+        static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; //互斥锁的初始化
+        if(nullptr == svr){
+            pthread_mutex_lock(&lock);
+            if(nullptr == svr){
+                svr = new TcpServer(port);
+            }
+            pthread_mutex_unlock(&lock);
+        }
+        return svr;
+    }
+
+    void InitServer(){
+        Socket();
+        Bind();
+        Listen();
+        LOG(INFO , "tcp_server init .... success");
+    }
+
+    void Socket(){
+        listen_sock = socket(AF_INET , SOCK_STREAM , 0);
+        if(listen_sock < 0){
+            LOG(FATAL , "socket error");
+            exit(1);
+        }
+        int opt = 1;
+        setsockopt(listen_sock , SOL_SOCKET , SO_REUSEADDR , &opt , sizeof(opt));
+        LOG(INFO , "create socket ... success");
+    }
+
+    void Bind(){
+        struct sockaddr_in local;
+        memset(&local , 0 , sizeof(local));
+        local.sin_family = AF_INET;
+        local.sin_port = htons(port);
+        local.sin_addr.s_addr = INADDR_ANY;
+
+        if(bind(listen_sock , (struct sockaddr*)&local , sizeof(local)) < 0){
+            LOG(FATAL , "bind error");
+            exit(2);
+        }
+        LOG(INFO , "bind socket .... success");
+    }
+
+    void Listen(){
+        if(listen(listen_sock , 5) < 0){
+            LOG(FATAL , "listen socket error");
+            exit(3);
+        }
+        LOG(INFO , "listen socket ... success");
+    }
+
+    int getSock(){
+        return listen_sock;
+    }
+
+    ~TcpServer(){
+        if(listen_sock >= 0)
+            close(listen_sock);
+    }
+};
+
+TcpServer* TcpServer::svr = nullptr;
