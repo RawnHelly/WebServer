49dc089e7061f4fde38ef6b63366b7905daeceb0
Rawn <liu_826475@163.com>
2023-09-07 13:56:45 +0800

    增加了TcpServer的类

--- Socket/TcpServer.cpp
+++ Socket/TcpServer.cpp
@@ -1,3 +1,4 @@
+/*create a class for tcp*/
 #pragma once
 
 #include<sys/socket.h>
@@ -12,75 +13,74 @@
 
 #include "Log.hpp"
 
-class TcpServer {
-private:
-    int port;
-    int listen_sock;
-    static TcpServer *svr;
-private:
-    TcpServer(int _port): port(_port) , listen_sock(-1) {}
-    TcpServer(const TcpServer &s){}
-public:
-    static TcpServer *getinstance(int port) { //单例模式
-        static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; //互斥锁的初始化
-        if(nullptr == svr){
-            pthread_mutex_lock(&lock);
-            if(nullptr == svr){
-                svr = new TcpServer(port);
+class TcpServer{
+    private:
+        int port;
+        int listen_fd;
+        static TcpServer* server_fd;
+    private:
+        TcpServer(int _port) : port(_port) , listen_fd(-1){} //这里我们将构造函数定义为private，这样子类就无法继承
+        TcpServer(const TcpServer &s){}
+    public:
+
+        void init();
+        void Socket();
+        void Listen();
+        void Bind();
+        int Sock(); //返回链接的套接字
+        static TcpServer *getinstance(int port){
+            static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+            if(server_fd == nullptr){
+                pthread_mutex_lock(&lock); // 先加锁
+                server_fd = new TcpServer(port);
+                server_fd->init();
+                pthread_mutex_unlock(&lock);
             }
-            pthread_mutex_unlock(&lock);
+
+            return server_fd;
         }
-        return svr;
-    }
 
-    void InitServer(){
-        Socket();
-        Bind();
-        Listen();
-        LOG(INFO , "tcp_server init .... success");
-    }
-
-    void Socket(){
-        listen_sock = socket(AF_INET , SOCK_STREAM , 0);
-        if(listen_sock < 0){
-            LOG(FATAL , "socket error");
-            exit(1);
+        void init(){
+            Socket(); //创建
+            Bind(); //绑定
+            Listen(); //监听
         }
-        int opt = 1;
-        setsockopt(listen_sock , SOL_SOCKET , SO_REUSEADDR , &opt , sizeof(opt));
-        LOG(INFO , "create socket ... success");
-    }
 
-    void Bind(){
-        struct sockaddr_in local;
-        memset(&local , 0 , sizeof(local));
-        local.sin_family = AF_INET;
-        local.sin_port = htons(port);
-        local.sin_addr.s_addr = INADDR_ANY;
-
-        if(bind(listen_sock , (struct sockaddr*)&local , sizeof(local)) < 0){
-            LOG(FATAL , "bind error");
-            exit(2);
+        void Socket(){
+            listen_fd = socket(AF_INET , SOCK_STREAM , 0);
+            if(listen_fd < 0)
+                exit(1);
+            int opt = 1;
+            setsockopt(listen_fd , SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
         }
-        LOG(INFO , "bind socket .... success");
-    }
 
-    void Listen(){
-        if(listen(listen_sock , 5) < 0){
-            LOG(FATAL , "listen socket error");
-            exit(3);
+        void Bind(){
+            struct sockaddr_in local;
+            memset(&local, 0, sizeof(local));
+            local.sin_family = AF_INET;
+            local.sin_port = htons(port);
+            local.sin_addr.s_addr = INADDR_ANY; //云服务器不能直接绑定公网IP
+
+            if(bind(listen_fd, (struct sockaddr*)&local, sizeof(local)) < 0){
+                LOG(FATAL, "bind error!");
+                exit(2);
+            }
         }
-        LOG(INFO , "listen socket ... success");
-    }
 
-    int getSock(){
-        return listen_sock;
-    }
+        void Listen(){
+            if(listen(listen_fd , 5) < 0){
+                exit(3);
+            }
+        }
 
-    ~TcpServer(){
-        if(listen_sock >= 0)
-            close(listen_sock);
-    }
+        int Sock(){
+            return listen_fd;
+        }
+
+        ~TcpServer(){
+            if(listen_fd > 0)
+                close(listen_fd);
+        }
 };
 
-TcpServer* TcpServer::svr = nullptr;
+TcpServer* TcpServer::server_fd = nullptr;
